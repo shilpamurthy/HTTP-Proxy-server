0588829c86faecab9297624c88d2d11dec140c59
shilpamurthy <shilpamurthy93@gmail.com>
2014-04-27 23:01:30 -0400

    "Part 1"

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -13,8 +13,6 @@ static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64;
 static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
 
-//#define SAFE_SSCANF(function)( if(function == EOF) {printf("Error in scanning input in %s, line %d\n", __FILE__, __LINE__); return;})
-//#define SAFE_RIO(function) (if(function < 0) { printf("Error in RIO usage, in %s. line %d\n", __FILE__, __LINE__); return; })
 
 void doit(int fd);
 int safe_scan(int n);
@@ -30,15 +28,16 @@ int main(int argc, char **argv)
 	int listenfd, connfd, clientlen, port;
 	struct sockaddr_in clientaddr;
 
+
 	port = atoi(argv[1]);
 
-
 	listenfd = Open_listenfd(port);
+
 
 	while (1)
 	{
 		clientlen = sizeof(clientaddr);
-		connfd = Accept(listenfd, (SA *)&clientaddr, (unsigned int *)&clientlen);
+		connfd = Accept(listenfd,(SA *)&clientaddr,(unsigned int *)&clientlen);
 		doit(connfd);
 		Close(connfd);
 	}
@@ -50,12 +49,14 @@ int main(int argc, char **argv)
 void doit(int fd)
 {
 	int isnot_safe;
-	char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE], temp[MAXLINE], requestbuf[MAXLINE], host[MAXLINE], headers[MAXLINE], path[MAXLINE];
-	char *data = calloc(MAX_OBJECT_SIZE, sizeof(char));
+	char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE], 
+	temp[MAXLINE], requestbuf[MAXLINE], host[MAXLINE], headers[MAXLINE], 
+	path[MAXLINE];
+	char *data = malloc(MAX_OBJECT_SIZE*sizeof(char));
 	size_t dataLen = 0;
-	char *dataBuffer = calloc(MAX_OBJECT_SIZE, sizeof(char));
+	char *dataBuffer = malloc(MAX_OBJECT_SIZE*sizeof(char));
 	char* port_pointer;
-	int port = 80, len, len1;
+	int port = 80, len;
 	rio_t rio, rio1;
 	int serverConn_fd;
 
@@ -71,7 +72,7 @@ void doit(int fd)
 		printf("501 Not Implemented Proxy does not handle this method");
 		return;
     }
-    printf("Does it reach here after Get?\n");
+    //printf("Does it reach here after Get?\n");
 
     if (strstr(uri, "https://") || strstr(uri, "http://"))
     {
@@ -112,28 +113,31 @@ void doit(int fd)
     serverConn_fd = Open_clientfd(host, port);
     Rio_readinitb(&rio1, serverConn_fd);
 
-    isnot_safe = safe_rio(rio_writen(serverConn_fd, requestbuf, strlen(requestbuf)));
+    isnot_safe = safe_rio(rio_writen(serverConn_fd, requestbuf, 
+    						strlen(requestbuf)));
 
     if (isnot_safe)
         return;
 
-    while((len = rio_readlineb(&rio, requestbuf, MAXBUF)) && strcmp(requestbuf, "\r\n"))
+    while((len = rio_readlineb(&rio, requestbuf, MAXBUF)) && 
+    			strcmp(requestbuf, "\r\n"))
     {
 	    isnot_safe = safe_scan(sscanf(requestbuf, "%s ",headers));
 
         if (isnot_safe)
         	return;
 
-	    if(strcmp(headers, "Host:") != 0 && strcmp(headers, "Connection:") != 0 &&
-	       strcmp(headers, "User-Agent:") != 0 && strcmp(headers, "Accept-Encoding:") != 0
-	       && strcmp(headers,"Accept:") != 0 && strcmp(headers,"Proxy-Connection:") != 0){
+	    if(strcmp(headers, "Host:") && strcmp(headers, "Connection:") &&
+	       strcmp(headers, "User-Agent:") && strcmp(headers,"Accept-Encoding:")
+	       && strcmp(headers,"Accept:") && strcmp(headers,"Proxy-Connection:"))
+	    {
 
 	        isnot_safe = safe_rio(rio_writen(serverConn_fd, requestbuf, len));
 			
         	if (isnot_safe)
         		return;	   	
-    }
-  }
+    	}
+  	}
 
   	strcpy(requestbuf, user_agent_hdr);
   	isnot_safe = safe_rio(rio_writen(serverConn_fd, requestbuf, strlen(requestbuf)));
@@ -153,21 +157,20 @@ void doit(int fd)
     if (isnot_safe)
         return;
 
-  	isnot_safe = safe_rio(rio_writen(serverConn_fd,"Proxy-Connection: close\r\nConnection:\t"
-                                      " close\r\n\r\n", 52));
+  	isnot_safe = safe_rio(rio_writen(serverConn_fd,
+  		"Proxy-Connection: close\r\nConnection:\tclose\r\n\r\n", 52));
 
     if (isnot_safe)
         return;
 
-  //finally display website
-  while((len1= rio_readnb(&rio1, data, len1)) != 0){
-    dataLen += len1;
+  while((len = rio_readnb(&rio1, data, len)) != 0){
+    dataLen += len;
 
     if (MAX_OBJECT_SIZE > dataLen){
-      memcpy(dataBuffer,data,len1);
-      dataBuffer += len1;
+      memcpy(dataBuffer,data,len);
+      dataBuffer += len;
     }
-    rio_writen(fd, data, len1);
+    rio_writen(fd, data, len);
   }
 
   free(data);
